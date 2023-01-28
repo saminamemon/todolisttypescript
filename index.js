df28c6ccdce3ac5be97751349e8694a484f5f956
Samina Memon <119938256+saminamemon@users.noreply.github.com>
2023-01-28 19:31:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkAnimation.neon("lets start  'TO DO LIST'");
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+let todolist = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "what do you want to do:?"
+        },
+        {
+            type: "input",
+            name: "addmore",
+            message: " Do you want to add more ?",
+            default: false
+        },
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if (TODO) {
+        todolist.push(TODO);
+    }
+    else {
+        console.log("Kindly Add Valid Input");
+    }
+}
+if (todolist.length > 0) {
+    console.log("Your Todo List:");
+    todolist.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No Todos Found!");
+}
---
+++ index.ts
@@ -0,0 +1,60 @@
+#!/user/bin/env node
+import  inquirer  from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { loadavg } from "os";
+
+
+const sleep =()=>{
+    return new Promise((res)=>{
+        setTimeout(res, 2000)
+    })
+}
+async function welcome(){
+    let rainbowTitle= chalkAnimation.neon("lets start  'TO DO LIST'");
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome()
+ 
+     let todolist: string[] = [];
+    let loop = true;
+    
+    while (loop) {
+        const answers: {
+             TODO: string,
+             addmore: boolean
+        } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "TODO",
+                message: "what do you want to do:?"
+            },
+            {
+                type: "input",
+                name: "addmore",
+                message: " Do you want to add more ?",
+                default: false
+            },
+        ])
+        const { TODO, addmore } = answers;
+        console.log(answers);
+        
+        loop = addmore
+        if (TODO) {
+            todolist.push(TODO)
+        } else {
+            console.log("Kindly Add Valid Input");
+        }
+    }
+    
+    if (todolist.length > 0) {
+        console.log("Your Todo List:");
+        todolist.forEach(todo => {
+            console.log(todo);
+            
+        });
+    } else {
+        console.log("No Todos Found!");
+        
+    }
